4937ac9dd0bdc56cc4d3ca82f55f3db143b0743a
sselman <sselman@twitter.com>
2017-08-28 10:23:28 -0700

    fix validation message

--- src/main/java/app/Validations/CheckStringVal.java
+++ src/main/java/app/Validations/CheckStringVal.java
@@ -9,6 +9,7 @@ import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
 
 @Target({ FIELD, METHOD, PARAMETER, ANNOTATION_TYPE })
 @Retention(RUNTIME)
--- src/main/java/app/data/BidRepository.java
+++
@@ -1,8 +0,0 @@
-package app.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BidRepository extends CrudRepository<BidEntity, String> {
-}
